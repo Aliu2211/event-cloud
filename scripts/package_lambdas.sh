2110d6be94621ad07b5a96c1161442a083aae503
Aliu Tijani <aliutijani21@gmail.com>
2026-08-05 12:33:39 +0000

    chore: package all Lambda functions for deployment

--- scripts/package_lambdas.sh
+++ scripts/package_lambdas.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-FUNCTIONS=(event_create event_get event_list registration_create registration_get registration_list)
+FUNCTIONS=(event_create event_get event_list registration_create registration_get registration_delete registration_list registration_list_all registration_lookup_by_email session_create session_list speaker_create speaker_list speaker_get session_list_by_speaker image_upload_url)
 mkdir -p infrastructure/packages
 
 for FUNC in "${FUNCTIONS[@]}"; do
   echo "Packaging $FUNC..."
   cd functions/$FUNC
-  pip install -r requirements.txt -t ./package --quiet
+  python3 -m pip install -r requirements.txt -t ./package --quiet
   cp handler.py ./package/
   cd package && zip -r ../../../infrastructure/packages/${FUNC}.zip . --quiet
   cd .. && rm -rf package && cd ../..
